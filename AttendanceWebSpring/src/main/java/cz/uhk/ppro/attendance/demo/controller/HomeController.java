102d72ddce8cdedb3e301cfd63ba38119942cdc8
chaloma1 <34541767+chaloma1@users.noreply.github.com>
2019-01-25 00:54:17 +0100

    First commit
    First setup with functional jsp page and database connection.

---
+++ AttendanceWebSpring/src/main/java/cz/uhk/ppro/attendance/demo/controller/HomeController.java
@@ -0,0 +1,20 @@
+package cz.uhk.ppro.attendance.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HomeController {
+
+    @RequestMapping("/")
+    @ResponseBody
+    public String action(){
+        return "Ahoj!";
+    }
+
+    @RequestMapping("/index")
+    public String helloworld() {
+        return "index";
+    }
+}
